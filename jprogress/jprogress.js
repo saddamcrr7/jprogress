4acb23049f0ce9cc5357dc0093b71ac20187541a
Robin Islam <r0b1n1sl4m@gmail.com>
2015-10-17 23:54:39 +0600

    Done with plugin initial method

--- jprogress/jprogress.js
+++ jprogress/jprogress.js
@@ -1,7 +1,30 @@
 (function($) {
+    // start writing function
     $.fn.jprogress = function() {
+        
+        // return everytime when plugin function get called
         return this.each(function() {
-            $(this).addClass("testing");
+            
+            // define plugin obj
+            var mainobj = $(this);
+            
+            // create method
+            progressmethod = {
+                //progress init
+                init: function(bar) {
+                    if(bar != 'undefined') {
+                        $(mainobj).addClass('progress_single');
+                        $(mainobj).wrap('<div class="progress_single_wrapper"></div>');
+                    }
+                }
+            };
+            
+            //check if object is exist
+            if(mainobj.length > 0) {
+                // call init method
+                progressmethod.init();
+            }
+            
         });
     }
 }(jQuery));
